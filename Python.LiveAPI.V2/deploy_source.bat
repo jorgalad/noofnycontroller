ebbd686afcca60141e9111c6c177d4d9d51c02b4
noofny <noofny@e681a0fc-d3d2-11de-81c4-1f0d5e5399c3>
2009-11-21 07:05:30 +0000

    .

--- Python.LiveAPI.V2/deploy_source.bat
+++ Python.LiveAPI.V2/deploy_source.bat
@@ -1,6 +1,6 @@
 c:
 del "C:\Program Files (x86)\Ableton\Live 8.0.4\Resources\MIDI Remote Scripts\NoofnyController\*.*" /F /Q
-copy /Y "E:\Workspace\NoofnyControllerV2\src\*.py" "C:\Program Files (x86)\Ableton\Live 8.0.4\resources\midi remote scripts\NoofnyController\"
+copy /Y "E:\Workspace\NoofnyController\Python.LiveAPI.V2\src\*.py" "C:\Program Files (x86)\Ableton\Live 8.0.4\resources\midi remote scripts\NoofnyController\"
 del c:\NoofnyController.log /F /Q
 rem cmd /c "C:\Program Files (x86)\Ableton\Live 8.0.4\program\Live 8.0.4.exe"
 cmd /c "E:\Songs\Noofny.ProgSet\Noofny.ProgSet.als"
